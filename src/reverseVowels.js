9c0ed32b371b118b5ff90b9e952b4d0b9922eb1e
Kingcedru <byishimocedrick@gmail.com>
2023-10-23 13:01:16 +0200

    reverse vowels

---
+++ src/reverseVowels.js
@@ -0,0 +1,29 @@
+function reverseVowels(str) {
+    const vowels = ['a','e','i','o','u']
+    str.split(' ').join(',').split('')
+    const onlyValues = str.split('').filter((item,index)=>{
+      if(vowels.includes((item.toUpperCase()) && (item.toLowerCase()))){
+        return item
+      }
+    }).reverse()
+  
+    const nonVowelsNumbers = str.split(' ').join(',').split('').map((item,index)=> vowels.includes((item.toUpperCase()) && (item.toLowerCase()))?index:item)
+  
+    let i = 0
+    let t = -1
+    let reversedVowels = []
+    while (i < nonVowelsNumbers.length){
+      if(typeof(nonVowelsNumbers[i])== 'number'){
+        reversedVowels.push(onlyValues[t+1])
+       t+=1
+      }
+      else{
+        reversedVowels.push(nonVowelsNumbers[i])
+      }
+      i+= 1
+      }
+    return reversedVowels.join('').split(',').join(' ')
+  }
+  reverseVowels("Hello!")//"Holle!"
+  reverseVowels("Tomatoes")// "Temotaos"
+  reverseVowels("Reverse Vowels In A String")//"RivArsI Vewols en e Streng"
