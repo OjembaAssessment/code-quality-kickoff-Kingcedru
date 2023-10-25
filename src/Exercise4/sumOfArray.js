3506501560f3e24819c8f3b285cf7fe982616375
Kingcedru <byishimocedrick@gmail.com>
2023-10-25 12:02:06 +0200

    making changes

--- src/Exercise4/sumOfArray.js
+++ src/Exercise4/sumOfArray.js
@@ -1,4 +1,5 @@
-function addingArray(arr){
-    return arr.flat(Infinity).reduce((sum,occurs)=> sum + occurs)
- }
- addingArray([1,2,[3,4],[5,[[[[[[6]]]]]],6]])
+function recursiveArraySum(arr, index) {
+    return index < 0 ? 0:arr[index] + recursiveArraySum(arr, index - 1);
+  }
+  const numbers = [1, 2, 3, 4, 5];
+  recursiveArraySum(numbers,numbers.length - 1)
