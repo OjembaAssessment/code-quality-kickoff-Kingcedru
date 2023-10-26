1bdc4741f95bdac689d03dc3f6742682ecaa6d0b
Kingcedru <byishimocedrick@gmail.com>
2023-10-26 12:14:45 +0200

    up

--- src/Exercise5/fibonacci.js
+++ src/Exercise5/fibonacci.js
@@ -1,10 +1,12 @@
 const fibonacciArray = (size) => {
   let fibonacci = [0, 1];
   let sum = 0;
-  for (let x = 2; x <= size; x++) {
-    sum = fibonacci[x - 1] + fibonacci[x - 2];
-    fibonacci.push(sum);
+  for (let x = 2; x <= size + 1; x++) {
+    if (sum < size) {
+      sum = fibonacci[x - 1] + fibonacci[x - 2];
+      fibonacci.push(sum);
+    }
   }
-  return fibonacci;
+  return fibonacci.filter((item) => item <= size);
 };
-fibonacciArray(2);
+fibonacciArray(4);
